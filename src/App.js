be60e57620b012d942ae44f1b30969bb37a1a250
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-10-10 05:03:22 -0400

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Admin from "./admin";
+import Home from "./home";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route path="/admin">
           <Admin />
+        </Route>
+        <Route path="/">
+          <Home />
         </Route>
       </Switch>
     </Router>
