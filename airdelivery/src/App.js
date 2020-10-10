9f7c4b8d61428648c2984cd154f19cb4d03dc4f8
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-10-09 20:46:05 -0400

    Created a user input component

--- airdelivery/src/App.js
+++ airdelivery/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import UserInput from "./Components/user-input";
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <UserInput></UserInput>
       </header>
     </div>
   );
