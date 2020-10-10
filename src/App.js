bcdc1eac165e0057a3159fe71b697c029296be82
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-10-10 05:07:42 -0400

    removed routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Admin from "./admin";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Admin />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <Admin />;
 }
 
 export default App;
