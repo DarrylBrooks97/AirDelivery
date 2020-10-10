ea062eed11f5ee91628349775bd8d00d5e357adb
Darryl Brooks <darryl.brooks@myfuelmaster.com>
2020-10-10 05:02:08 -0400

    wi

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Admin from "./admin";
-import Home from "./home";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/admin">
-          <Admin />
-        </Route>
         <Route path="/">
-          <Home />
+          <Admin />
         </Route>
       </Switch>
     </Router>
